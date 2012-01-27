fc90d7b6a1eb01836922c007ee1f16ee16144a16
Lewis Strong <L.Strong@2009.ljmu.ac.uk>
2012-01-27 15:18:13 +0000

    Finished

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ActiveRecord::Base
   validates_presence_of :leavedate, :returndate, :total
-  attr_accessible :leavedate, :returndate, :total
+  attr_accessible :leavedate, :returndate, :total, :authorised
   belongs_to :user
 end
